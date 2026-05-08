e441ac196f6a05929c5383941feaf4169736568d
josechepeg <147000955+josechepeg@users.noreply.github.com>
2026-05-08 22:38:11 +0000

    feat: add PUT pedidos endpoint

--- src/controllers/pedidos.controller.js
+++ src/controllers/pedidos.controller.js
@@ -20,6 +20,52 @@ const getPedidoById = (req, res) => {
   res.status(200).json(pedido);
 };
 
+const updatePedido = (req, res) => {
+
+  const { id } = req.params;
+
+  const { cliente, producto, cantidad, total } = req.body;
+
+  const pedido = pedidos.find(p => p.id === id);
+
+  if (!pedido) {
+    return res.status(404).json({
+      mensaje: 'Pedido no encontrado'
+    });
+  }
+
+  // VALIDACIONES
+
+  if (!cliente || !producto || !cantidad || !total) {
+    return res.status(400).json({
+      error: 'Todos los campos son obligatorios'
+    });
+  }
+
+  if (cantidad <= 0) {
+    return res.status(400).json({
+      error: 'La cantidad debe ser mayor a 0'
+    });
+  }
+
+  if (total <= 0) {
+    return res.status(400).json({
+      error: 'El total debe ser mayor a 0'
+    });
+  }
+
+  pedido.cliente = cliente;
+  pedido.producto = producto;
+  pedido.cantidad = cantidad;
+  pedido.total = total;
+
+  res.status(200).json({
+    mensaje: 'Pedido actualizado correctamente',
+    pedido
+  });
+
+};
+
 // POST pedido
 const createPedido = (req, res) => {
 
@@ -64,5 +110,6 @@ const createPedido = (req, res) => {
 module.exports = {
   getPedidos,
   createPedido,
-  getPedidoById
+  getPedidoById,
+  updatePedido
 };
--- src/routes/pedidos.js
+++ src/routes/pedidos.js
@@ -4,12 +4,16 @@ const router = express.Router();
 const {
   getPedidos,
   createPedido,
-  getPedidoById
+  getPedidoById,
+  updatePedido
 } = require('../controllers/pedidos.controller');
 
 // RUTAS
 router.get('/', getPedidos);
 router.get('/:id', getPedidoById);
+router.put('/:id', updatePedido);
 router.post('/', createPedido);
+
+
 
 module.exports = router;
